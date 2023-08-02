d1a495cdd14b79a7dbd361f192853320ab240bb6
thetravelsquare <server.thetravelsquare@gmail.com>
2023-08-02 11:10:56 +0530

    color change and border update of the table in print members

--- resources/views/members/members.blade.php
+++ resources/views/members/members.blade.php
@@ -20,32 +20,33 @@
           </div>
             <div class="row">
                 <div class="col-md-12">
-                    <table id="table_name">
+                    <table id="table_name" >
                         <tr>
-                            <td style="width: 100px; background: #fff; border: 1px solid #000"><img src="{{ asset('assets/img/meena.jpeg') }}" style="width: 96px; height: 100px;" alt=""></td>
-                            <td colspan="2" style="width: 100%; background: #fff; border: 1px solid #000; border-left: 1px solid #000;"><h2 class="text-center">टोडाभीम <br>विधान सभा छेत्र (संख्या-81)</h2></td>
+                            <td style="width: 100px; background: #ddd; border: 1px solid #000"><img src="{{ asset('assets/img/meena.jpeg') }}" style="width: 96px; height: 100px;" alt=""></td>
+                            <td colspan="2" style="width: 100%; background: #ddd; border: 1px solid #000; border-left: 1px solid #000;"><h2 class="text-center">टोडाभीम <br>विधान सभा छेत्र (संख्या-81)</h2></td>
                             <td style="width: 100px; background: #fff; border: 1px solid #000"><img src="{{ asset('assets/img/flag.svg') }}" style="width: 96px; height: 100px;">
                         <tr style="border: 1px solid #000;">
-                            <td style="width: 100px; background: #fff;"></td>
-                            <td colspan="2" style="width: 100%; background: #fff;"><p class="text-center mt-1"><b>पोलिंग बूथ कार्यकर्ता विवरण</b></p></td>
-                            <td style="width: 100px; background: #fff;"></td>
+                            <td style="width: 100px; background: #6bbdfa;"></td>
+                            <td colspan="2" style="width: 100%; background: #6bbdfa; color: #eee;"><p class="text-center mt-1"><b>पोलिंग बूथ कार्यकर्ता विवरण</b></p></td>
+                            <td style="width: 100px; background: #6bbdfa;"></td>
                         </tr>
                         @php $i = 1 @endphp
                         @foreach($members->chunk(2) as $chunk_member)
                         @once
-                        <tr style="border: 1px solid #000;">
-                            <td colspan="2" style="width: 100%; background: #fff;"><p class="mt-1 mx-3"><b>बूथ-{{ $chunk_member[0]['booth_id']}} {{ App\Models\Booth::where('id', $chunk_member[0]['booth_id'])->value('name') }}</b></p></td>
-                            <td style="width: 100px; background: #fff;"></td>
-                            <td style="width: 100px; background: #fff;"></td>
+                        <tr style="border-right: 1px solid #000; border-left: 1px solid #000;">
+                            <td colspan="2" style="width: 100%; background: #ddd;"><p class="mt-1 mx-3"><b>बूथ-{{ $chunk_member[0]['booth_id'] }} {{ App\Models\Booth::where('id', $chunk_member[0]['booth_id'])->value('name') }}</b> &nbsp; <span class="hidden-print">({{ count(App\Models\Member::where('booth_id', $chunk_member[0]['booth_id'])->get()) }} Members)</span></p></td>
+                            <td style="width: 100px; background: #ddd;"></td>
+                            <td style="width: 100px; background: #ddd;"></td>
                         </tr>
                         @endonce
                         <tr style="margin: 0px;">
                             @foreach($chunk_member as $member)
                             @php $i++ @endphp
                             <td colspan="2">
-                                <table>
+                                <table style="margin: -2px;margin-top: -1px; margin-right: -1px;">
                                     <tr>
                                         <td style="width: 700px; border: 1px solid #000;">
+                                            {{-- <b class="hidden-print">BoothId: {{ $member->booth_id }}</b> --}}
                                             <div class="p-3">
                                                 <b>नाम: {{ $member->name }}</b> <br>
                                                 <b>नंबर: {{ $member->phone }}</b> <br>
@@ -61,11 +62,11 @@
                                 </table>
                             </td>
                             @if($i == 7) 
-                            <tr style="border: 1px solid #000; width: 100%">
-                                <td colspan="2" style="width: 100%; background: #fff;"><p class="mt-1 mx-3"><b>बूथ-{{ $member->booth_id +1}} {{ App\Models\Booth::where('id', $member->booth_id + 1)->value('name') }}</b> </p></td>
+                            <tr style="border-right: 1px solid #000; border-left: 1px solid #000; border-top: 1px solid #000; width: 100%; margin-right: 1px;">
+                                <td colspan="2" style="width: 100%; background: #ddd;"><p class="mt-1 mx-3"><b>बूथ-{{ $member->booth_id }} {{ App\Models\Booth::where('id', $member->booth_id )->value('name') }}</b> &nbsp;  <span class="hidden-print">({{ count(App\Models\Member::where('booth_id', $member->booth_id )->get()) }} Members)</span></p></td>
                                 {{-- <td colspan="2" style="width: 100%; background: #fff;"><p class="mt-1">पोलिंग बूथ कार्यकर्ता विवरण {{ $member->booth_id +1}}</p></td> --}}
-                                <td style="width: 100px; background: #fff;"></td>
-                                <td style="width: 100px; background: #fff;"></td>
+                                <td style="width: 100px; background: #ddd;"></td>
+                                <td style="width: 100px; background: #ddd;"></td>
                             </tr>
                             @endif
                         @endforeach
